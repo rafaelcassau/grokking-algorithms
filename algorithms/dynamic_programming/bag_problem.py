aeaee06a36cb479f26176b0a77e46ad1a615a228
Rafael Cassau <rafa.cassau@gmail.com>
2019-10-29 00:16:28 -0300

    add more dynamic programming explanation

--- algorithms/dynamic_programming/bag_problem.py
+++ algorithms/dynamic_programming/bag_problem.py
@@ -1,38 +1,71 @@
 """
-Imagine you are a thief and you have a bag that can fill 4KG and you want to stole
-a store, inside the store we have some items:
+Theory:
+    The dynamic programming is useful when you're trying to optimize something against a threshold.
 
-1 - Guitar   - $ 1.500 1KG
-2 - Radio    - $ 3.000 4KG
-3 - Notebook - $ 2.000 3KG
+    You can use dynamic programming when a problem can be divided into discret () subproblems.
 
-You want to fill the bag with the most valueble items, how do you do that?
+    All dynamic programming solutions involve a table.
 
-The best solution requires that you calcule each subset of items for all possibilities and
-chose the best one that fill the bag with the most expensive items, the problem behind this
-approach is the huge amount of possibilities, to be more expecifically O(2^N) or O(N!),
-see bellow some examples:
+    Values in cells are usually what you are trying to optimize.
 
-    3  items 2^3  = 8          possibilities
-    4  items 2^4  = 16         possibilities
-    5  items 2^5  = 32         possibilities
-    6  items 2^6  = 64         possibilities
-    32 items 2^32 = 4 billions possibilities
+    Each cell is a subproblem, so think about how you can split this subproblem into other subproblems.
 
-To solve this problem we can use two different approaches, the first one is greedy algorithms
-with this approach we can find a approximately good solution, but the best one will be reach
-using dynamic programing.
+    There is no single formula for calculating a dynamic programming solution.
+    
 
-Dynamic programing is a technique that divide the complex problem in a small set of sub problems
-and resolve each subproblem progressively, in each iterator we solve a next subproblem,
-when the iterator ends we have reached the best solution.
+Real world examples:
+    Biologists uses the largest common subsequence to find similarities in DNA, to know how
+    similar two animals or two diseases are. The same technique is being used to figure out
+    multiple sclerosis cure.
 
-The secret is how the way that you model the problem inside the table.
+    Have you ever used the git diff command? This command tells the difference between two
+    files using dynamic programming.
+
+    We talked about similarities between strings. The Levenshtein distance measure how much
+    similar two strings are using dynamic programming for that. The Levenshtein distance is
+    used simple spell checker as for figure out if a user is doing a upload with copyright
+    associated.
+
+    Have you ever used some software that break lines as Microsoft World? How a software knows
+    how to break the line to maintain the lines with the same? Dynamic programming is the answer.
 
 
-link with a good explanation: 
+Bag problem:
 
-https://youtu.be/F-wu-wkdv-M
+    Imagine you are a thief and you have a bag that can fill 4KG and you want to stole
+    a store, inside the store we have some items:
+
+    1 - Guitar   - $ 1.500 1KG
+    2 - Radio    - $ 3.000 4KG
+    3 - Notebook - $ 2.000 3KG
+
+    You want to fill the bag with the most valueble items, how do you do that?
+
+    The best solution requires that you calcule each subset of items for all possibilities and
+    chose the best one that fill the bag with the most expensive items, the problem behind this
+    approach is the huge amount of possibilities, to be more expecifically O(2^N) or O(N!),
+    see bellow some examples:
+
+        3  items 2^3  = 8          possibilities
+        4  items 2^4  = 16         possibilities
+        5  items 2^5  = 32         possibilities
+        6  items 2^6  = 64         possibilities
+        32 items 2^32 = 4 billions possibilities
+
+    To solve this problem we can use two different approaches, the first one is greedy algorithms
+    with this approach we can find a approximately good solution, but the best one will be reach
+    using dynamic programing.
+
+    Dynamic programing is a technique that divide the complex problem in a small set of sub problems
+    and resolve each subproblem progressively, in each iterator we solve a next subproblem,
+    when the iterator ends we have reached the best solution.
+
+    The secret is how the way that you model the problem inside the table.
+
+
+    link with a good explanation: 
+
+    https://youtu.be/F-wu-wkdv-M
 """
 
 weights = (0, 1, 2, 3, 4)
